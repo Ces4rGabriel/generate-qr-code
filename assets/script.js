e542facdc81897f2aa33ed65fb5fe91a2a897f8b
Ces4rGabriel <cesargabrieldeapulO@gmail.com>
2023-10-25 12:50:54 -0300

    new changes index/qrcode

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ let button = document.querySelector('#button-enviar');
 function viewUrl() {
     let url = document.getElementById('input-text').value;
     let urlQrCode = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' + url + '&choe=UTF-8';
-    window.location.href = 'qrcode.html?url=' + encodeURIComponent(urlQrCode);
+    window.location.href = 'src/qrcode.html?url=' + encodeURIComponent(urlQrCode);
+    
 }
 
